48e87f8cfb263eed53912a6226d1b2dd0d5a1547
yogthot <yogthot@myon.moe>
2021-11-21 03:36:03 +0000

    initial commit

---
+++ bot.py
@@ -0,0 +1,57 @@
+
+import os
+import asyncio
+import discord
+
+from pixiv import Pixiv
+
+async def _safe_await(coro):
+    try:
+        await coro
+    except:
+        pass
+
+class PixivBot:
+    def __init__(self, token, cookie):
+        self.token = token
+        self.pixiv = Pixiv(cookie)
+        
+        self.client = discord.Client(guild_subscriptions=False)
+        self.client.event(self.on_message)
+    
+    async def start(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.client.start(self.token))
+    
+    async def close(self):
+        await _safe_await(self.client.close())
+    
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        
+        ids = self.pixiv.find_urls(message.content)
+        if len(ids) > 0:
+            async with message.channel.typing():
+                with self.pixiv.download_preview(ids[0]) as file:
+                    await message.channel.send('', file=discord.File(file.path, file.filename))
+            
+            await message.edit(suppress=True)
+
+
+bot = PixivBot(os.environ['DISCORD_TOKEN'], os.environ['PIXIV_COOKIE'])
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(bot.start())
+
+print('bot started')
+
+try:
+    loop.run_forever()
+    
+except KeyboardInterrupt:
+    print('\n\nbye')
+    
+finally:
+    loop.run_until_complete(bot.close())
+
---
+++ pixiv.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+import tempfile
+import json
+import zipfile
+import subprocess
+import requests
+
+
+class PixivFile(os.PathLike):
+    def __init__(self, path, filename):
+        self.path = path
+        self.filename = filename
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args):
+        os.remove(self.path)
+    
+    def __fspath__(self):
+        return self.path
+    
+    def __str__(self):
+        return self.filename
+
+
+class Pixiv:
+    POST_REGEXP = re.compile('https?:\/\/(?:www\.)?pixiv\.net\/(?:[a-zA-Z]{2}\/)?artworks\/(?P<post_id>\d+)', flags=re.IGNORECASE)
+
+    POST_GET_URL = 'https://www.pixiv.net/ajax/illust/{post_id}'
+    #POST_PAGES_URL = 'https://www.pixiv.net/ajax/illust/{post_id}/pages'
+    POST_UGOIRA_URL = 'https://www.pixiv.net/ajax/illust/{post_id}/ugoira_meta'
+    
+    def __init__(self, cookie):
+        self.http = requests.Session()
+        
+        self.http.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/82.0',
+            'Referer': 'https://www.pixiv.net/'
+        })
+        
+        cookie = requests.cookies.create_cookie(name='PHPSESSID', value=cookie)
+        self.http.cookies.set_cookie(cookie)
+    
+    def find_urls(self, text):
+        return self.POST_REGEXP.findall(text)
+    
+    def _download(self, url):
+        with self.http.get(url, stream=True) as resp:
+            fd, path = tempfile.mkstemp()
+            
+            with os.fdopen(fd, 'w+b') as file:
+                for chunk in resp.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            
+            return path
+    
+    def download_preview(self, post_id):
+        response = self.http.get(self.POST_GET_URL.format(post_id=post_id))
+        response.raise_for_status()
+        post = json.loads(response.text)['body']
+        
+        if post['illustType'] == 2:
+            # ugoira
+            response = self.http.get(self.POST_UGOIRA_URL.format(post_id=post_id))
+            response.raise_for_status()
+            ugoira_meta = json.loads(response.text)['body']
+            
+            ugoira_zip = PixivFile(self._download(ugoira_meta['src']), None)
+            frames = ugoira_meta['frames']
+            
+            fd, dst_path = tempfile.mkstemp()
+            
+            with ugoira_zip, \
+                tempfile.TemporaryDirectory() as tmp_dir, \
+                zipfile.ZipFile(ugoira_zip, 'r') as zip:
+                
+                zip.extractall(tmp_dir)
+                
+                inputs_path = f'{tmp_dir}/inputs.txt'
+                with open(inputs_path, 'w+') as inputs:
+                    for frame in frames:
+                        inputs.write('file ' + str(frame['file']) + '\n')
+                        inputs.write('duration ' + str(frame['delay'] / 1000) + '\n')
+                
+                arguments = [
+                    'ffmpeg',
+                    # input
+                    '-f', 'concat',
+                    '-i', str(inputs_path),
+                    
+                    # output
+                    '-f', 'webm',
+                    '-c:v', 'libvpx',
+                    '-b:v', '10M',
+                    '-an',
+                    '-vsync', '2',
+                    '-r', '1000',
+                    '-y',
+                    '-'
+                ]
+                
+                FNULL = open(os.devnull, 'w')
+                ffmpeg = subprocess.Popen(arguments, shell=False, stdin=FNULL, stdout=fd, stderr=subprocess.PIPE)
+                out, err = ffmpeg.communicate()
+                if ffmpeg.returncode:
+                    if err: print(err.decode('utf-8'), file=sys.stderr)
+            
+            return PixivFile(dst_path, f'{post_id}.webm')
+            
+            
+        else:
+            # get the first image and nothing else
+            url = post['urls']['regular']
+            return PixivFile(self._download(url), url.split('/')[-1])
+
+
+
